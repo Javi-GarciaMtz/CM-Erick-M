2930d0e6ebfe0caf1612f9c387a1ac9c00bedf99
Javi-GarciaMtz <javi.javier.gm@gmail.com>
2022-10-09 23:27:37 -0500

    1st try of models

---
+++ frontend/src/app/models/property.ts
@@ -0,0 +1,20 @@
+export class Property {
+
+    constructor(
+        public id: number,
+        public country: string,
+        public state: string,
+        public name: string,
+        public description_1: string,
+        public brochure: string,
+        public ubication: string,
+        public virtual_tour: string,
+        public lot_type: string,
+        public financing: string,
+        public delivery: string,
+        public master_plan: string,
+        public video: string,
+        public description_2: string,
+    ) {}
+
+}

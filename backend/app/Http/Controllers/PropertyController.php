9222cfd9dede8d9ed8fb78d70698ebcc11835ff1
Javi-GarciaMtz <javi.javier.gm@gmail.com>
2022-10-15 15:13:11 -0500

    api routes working

--- backend/app/Http/Controllers/PropertyController.php
+++ backend/app/Http/Controllers/PropertyController.php
@@ -4,11 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PropertyController extends Controller
-{
+class PropertyController extends Controller {
 
     public function get_property_by_id($id) {
         return "<h1>Regrese la propiedad con id: ".$id." y esta chidori!</h1>";
     }
+
+    public function get_location($id_property) {
+        return "<h1>Regrese la localizacion de la propiedad con id: ".$id_property." y esta chidori!</h1>";
+    }
+
+    public function get_virtual_tour($id_property) {
+        return "<h1>Regrese el recorrido virtual de la propiedad con id: ".$id_property." y esta chidori!</h1>";
+    }
+
+    public function get_surfaces_amenities($id_property) {
+        return "<h1>Regrese las superficies y amenidades de la propiedad con id: ".$id_property." y esta chidori!</h1>";
+    }
+
+    public function get_financing($id_property) {
+        return "<h1>Regrese el financiamiento de la propiedad con id: ".$id_property." y esta chidori!</h1>";
+    }
+
+    public function get_delivery($id_property) {
+        return "<h1>Regrese la entrega de la propiedad con id: ".$id_property." y esta chidori!</h1>";
+    }
+
+    public function get_mater_plan($id_property) {
+        return "<h1>Regrese el master plan de la propiedad con id: ".$id_property." y esta chidori!</h1>";
+    }
+
 
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -20,3 +20,19 @@ Route::get('/', function () {
 });
 
 Route::get('/api/property/{id}', [PropertyController::class, 'get_property_by_id']);
+
+Route::get('/api/location/{id_property}', [PropertyController::class, 'get_location']);
+
+Route::get('/api/virtual_tour/{id_property}', [PropertyController::class, 'get_virtual_tour']);
+
+Route::get('/api/surfaces_amenities/{id_property}', [PropertyController::class, 'get_surfaces_amenities']);
+
+Route::get('/api/financing/{id_property}', [PropertyController::class, 'get_financing']);
+
+Route::get('/api/delivery/{id_property}', [PropertyController::class, 'get_delivery']);
+
+Route::get('/api/mater_plan/{id_property}', [PropertyController::class, 'get_mater_plan']);
+
+
+// Route::post('/api/save_user', [::class, '']);
+// Route::get('/api/get_users', [::class, '']);

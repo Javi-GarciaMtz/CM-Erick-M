896eb4fa8660660e095ab05d18c8235fecd7c5d0
Javi-GarciaMtz <javi.javier.gm@gmail.com>
2022-10-20 23:41:00 -0500

    save and get users working

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -3,16 +3,74 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller {
 
-    public function save_user() {
-        return "<h1>Guardo un usuario</h1>";
+    public function save_user(Request $request) {
+        $json = $request->input('json', null);
+        $params_array = json_decode($json, true);
+
+        // Validar datos
+        $validate = \Validator::make($params_array, [
+            'name'      =>  'required|alpha',
+            'surname'   =>  'alpha',
+            'email'     =>  'required|email',
+            'phone'     =>  'required',
+        ]);
+
+        if($validate->fails()) {
+            $data = array(
+                'status'    =>  'error',
+                'code'  =>  404,
+                'message'   =>  "The user hasn't been created",
+                'errors'    =>  $validate->errors()
+            );
+
+        } else {
+            try {
+                $query_result = DB::insert('INSERT INTO users (id, name, surname, email, phone) values (?, ?, ?, ?, ?)', [null, $params_array['name'], $params_array['surname'], $params_array['email'], $params_array['phone']]);
+                $data = array(
+                    'code' => 200,
+                    'status' => 'Success',
+                    'msg' => 'Usuario almacenado correctamente.'
+                );
+
+            } catch (\Throwable $th) {
+                $data = array(
+                    'code' => 400,
+                    'status' => 'Error',
+                    'msg' => "Error al conectarse a la base de datos!"
+                );
+
+            }
+
+        }
+
+        return response()->json($data, $data['code']);
+
     }
 
     public function get_users() {
-        return "<h1>Regreso todos los usuarios</h1>";
-    }
+        try {
+            $users = DB::table('users')->get();
+            $data = array(
+                'code' => 200,
+                'status' => 'Success',
+                'users' => $users
+            );
 
+        } catch (\Throwable $th) {
+            $data = array(
+                'code' => 400,
+                'status' => 'Error',
+                'msg' => "Error al conectarse a la base de datos!"
+            );
+
+        }
+
+        return response()->json($data, $data['code']);
+
+    }
 
 }

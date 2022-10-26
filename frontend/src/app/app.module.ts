b60df1e0e130c6ba6fea7735be5903b0f34b3dec
Javi-GarciaMtz <javi.javier.gm@gmail.com>
2022-10-25 22:41:53 -0500

    components created, pending maybe 3 more

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,22 @@ import { routing, app_routing_providers } from "./app.routing";
 import { AppComponent } from './app.component';
 import { PropertyHomeComponent } from './components/property-home/property-home.component';
 import { HomeComponent } from './components/home/home.component';
+import { LocationComponent } from './components/location/location.component';
+import { AmenityComponent } from './components/amenity/amenity.component';
+import { FinancingComponent } from './components/financing/financing.component';
+import { DeliveryComponent } from './components/delivery/delivery.component';
+import { MasterPlanComponent } from './components/master-plan/master-plan.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PropertyHomeComponent,
-    HomeComponent
+    HomeComponent,
+    LocationComponent,
+    AmenityComponent,
+    FinancingComponent,
+    DeliveryComponent,
+    MasterPlanComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/amenity/amenity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-amenity',
+  templateUrl: './amenity.component.html',
+  styleUrls: ['./amenity.component.css']
+})
+export class AmenityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
